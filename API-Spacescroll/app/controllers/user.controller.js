8229728d166ae1c608b429c64a98d248e32271aa
VenancioC <a18475@alunos.ipca.pt>
2020-12-25 15:44:45 +0000

    1ª versão Rotas dos utilizadores CRUD

---
+++ API-Spacescroll/app/controllers/user.controller.js
@@ -0,0 +1,113 @@
+const User = require("../models/user.model.js");
+/*
+Validações e chamar os metodos do models
+*/
+
+// Create User
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  // Create a User
+  const user = new User({
+    Username: req.body.Username,
+    Name: req.body.Name,
+    Email: req.body.Email,
+    Password: req.body.Password,
+    BirthDate: req.body.BirthDate,
+    Genre: req.body.Genre
+  });
+
+  console.log(req.body);
+  console.log(user);
+
+  // Save Customer in the database
+  User.create(user, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the User."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all User from the database.
+exports.findAll = (req, res) => {
+  User.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Users."
+      });
+    else res.send(data);
+  });
+};
+
+// Find a single User with a userId
+exports.findOne = (req, res) => {
+  User.findById(req.params.userId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found User with id ${req.params.userId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving User with id " + req.params.userId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+// Update a User identified by the userId in the request
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  User.updateById(
+    req.params.userId,
+    new User(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found User with id ${req.params.userId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating User with id " + req.params.userId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+// Delete a User with the specified userId in the request
+exports.delete = (req, res) => {
+  User.remove(req.params.userId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found User with id ${req.params.userId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete User with id " + req.params.userId
+        });
+      }
+    } else res.send({ message: `User was deleted successfully!` });
+  });
+};
---
+++ API-Spacescroll/app/models/user.model.js
@@ -0,0 +1,119 @@
+const sql = require("./db_mysql.js");
+
+const bcrypt = require('bcrypt');
+const config = require('../config/config');
+/*
+Interação com a base de dados utilizando os dados recebidos 
+*/
+
+// constructor
+const User = function (user) {
+  this.Username = user.Username
+  this.Name = user.Name;
+  this.Email = user.Email;
+  this.Password = user.Password;
+  this.BirthDate = user.BirthDate;
+  this.Genre = user.Genre;
+};
+
+User.create = (newUser, result) => {
+  bcrypt.hash(newUser.Password, config.saltRounds, function (err, hash) {
+    newUser.Password = hash;
+    sql.query("INSERT INTO User (Username, Name, Email, Password, BirthDate, Genre) VALUES (?, ?, ?, ?, ?, ?)"
+      , [newUser.Username, newUser.Name, newUser.Email, newUser.Password, newUser.BirthDate, newUser.Genre]
+      , (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+
+        console.log("created user: ", { id: res.insertId, ...newUser });
+        result(null, { id: res.insertId, ...newUser });
+      });
+  });
+};
+
+User.getAll = result => {
+  sql.query("SELECT * FROM User"
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      console.log("Users: ", res);
+      result(null, res);
+    });
+};
+
+User.findById = (userId, result) => {
+  console.log(userId);
+  sql.query("SELECT * FROM User WHERE id =?"
+    , [userId]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+
+      if (res.length) {
+        console.log("found user: ", res);
+        result(null, res);
+        return;
+      }
+
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+    });
+};
+
+User.updateById = (id, user, result) => {
+  console.log(user);
+  bcrypt.hash(user.Password, config.saltRounds, function (err, hash) {
+    user.Password = hash;
+    sql.query("UPDATE User SET Username =?, Name =?, Email =?, Password =?, BirthDate =?, Genre =? WHERE id =?"
+      , [user.Username, user.Name, user.Email, user.Password, user.BirthDate, user.Genre, id]
+      , (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(null, err);
+          return;
+        }
+
+        if (res.affectedRows == 0) {
+          result({ kind: "not_found" }, null);
+          return;
+        }
+
+        console.log("updated user: ", { id: id, ...user });
+        result(null, { id: id, ...user });
+      }
+    );
+  });
+};
+
+User.remove = (id, result) => {
+  sql.query("DELETE FROM User WHERE id =?"
+    , [id]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("deleted user with id: ", id);
+      result(null, res);
+    });
+};
+
+module.exports = User;
