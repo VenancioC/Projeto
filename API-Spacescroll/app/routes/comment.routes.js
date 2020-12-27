45a8e76aa445d620146473e61198754e72367273
VenancioC <a18475@alunos.ipca.pt>
2020-12-27 16:07:15 +0000

    Comments CRUD e listar todos de um post

---
+++ API-Spacescroll/app/routes/comment.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+  const comments = require("../controllers/comment.controller.js");
+
+  // Create a new Comment
+  app.post("/comments", comments.create);
+
+  // Retrieve all Comments
+  app.get("/comments", comments.findAll);
+
+  // Retrieve a single Comment with commentId 
+  app.get("/comments/:commentId", comments.findOne);
+
+  // Retrieve Comments by PostId 
+  app.get("/comments/post/:postId", comments.findByPost);
+
+  // Update a Comment with commentId ->
+  app.put("/comments/:commentId", comments.update);
+
+  // Delete a Comment with commentId
+  app.delete("/comments/:commentId", comments.delete);
+};
