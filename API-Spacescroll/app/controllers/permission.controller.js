4d221915b2bdbb9421e2e6bed875bbf48d6a4a2c
VenancioC <a18475@alunos.ipca.pt>
2020-12-27 19:40:21 +0000

    Permission CRUD

---
+++ API-Spacescroll/app/controllers/permission.controller.js
@@ -0,0 +1,110 @@
+const Permission = require("../models/permission.model.js");
+
+/*
+Validações e chamar os metodos do models
+*/
+
+
+// Create and Save a new Category
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  // Create a Permission
+  const permission = new Permission({
+    Description: req.body.Description
+  });
+
+  console.log(req.body);
+  console.log(permission);
+
+  // Save Permission in the database
+  Permission.create(permission, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Permission."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Permissions from the database.
+exports.findAll = (req, res) => {
+  Permission.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Permissions."
+      });
+    else res.send(data);
+  });
+};
+
+// Find a single Permission with a permissionId
+exports.findOne = (req, res) => {
+  Permission.findById(req.params.permissionId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Permission with id ${req.params.categoryId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Permission with id " + req.params.categoryId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+// Update a Permission identified by the permissionId in the request
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  Permission.updateById(
+    req.params.permissionId,
+    new Permission(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Permission with id ${req.params.permissionId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating Permission with id " + req.params.permissionId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+// Delete a Permission with the specified permissionId in the request
+exports.delete = (req, res) => {
+  Permission.remove(req.params.permissionId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Permission with id ${req.params.permissionId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Permission with id " + req.params.permissionId
+        });
+      }
+    } else res.send({ message: `Permission was deleted successfully!` });
+  });
+};
---
+++ API-Spacescroll/app/models/permission.model.js
@@ -0,0 +1,106 @@
+const sql = require("./db_mysql.js");
+
+/*
+Interação com a base de dados utilizando os dados recebidos 
+*/
+
+
+// constructor
+const Permission = function (permission) {
+  this.Description = permission.Description;
+};
+
+Permission.create = (newPermission, result) => {
+  sql.query("INSERT INTO Permission (Description) VALUES (?)"
+    , [newPermission.Description]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      console.log("created Category: ", { id: res.insertId, ...newPermission });
+      result(null, { id: res.insertId, ...newPermission });
+    });
+};
+
+Permission.getAll = result => {
+  sql.query("SELECT * FROM Permission"
+  , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      console.log("Permissions: ", res);
+      result(null, res);
+  });
+};
+
+Permission.findById = (permissionId, result) => {
+  console.log(permissionId);
+  sql.query("SELECT * FROM Permission WHERE id =?"
+    , [permissionId]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+
+      if (res.length) {
+        console.log("found permissionId: ", res);
+        result(null, res);
+        return;
+      }
+
+      // not found Permission with the id
+      result({ kind: "not_found" }, null);
+    });
+};
+
+Permission.updateById = (id, permission, result) => {
+  console.log(permission);
+  sql.query("UPDATE Permission SET Description =? WHERE Id =?"
+    , [permission.Description, id]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated permission: ", { id: id, ...permission });
+      result(null, { id: id, ...permission });
+    }
+  );
+};
+
+Permission.remove = (id, result) => {
+  sql.query("DELETE FROM Permission WHERE id =?"
+    , [id]
+    , (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Permission with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("deleted permission with id: ", id);
+      result(null, res);
+    });
+};
+
+module.exports = Permission;
