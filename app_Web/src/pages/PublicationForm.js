b2b3d4b7459b3b8fe7312cb1f91c79beb35c16b6
BrunoFerr <58642813+BrunoFerr@users.noreply.github.com>
2021-01-18 23:48:11 +0000

    Eros correction in the publications creation page

--- app_Web/src/pages/PublicationForm.js
+++ app_Web/src/pages/PublicationForm.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import CreateIcon from '@material-ui/icons/Create';
+import CreateIcon from "@material-ui/icons/Create";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
 import { useFormik } from "formik";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import Cookies from "js-cookie";
-import Autocomplete from '@material-ui/lab/Autocomplete';
-
-
+import jwt from "jsonwebtoken";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import { useRouter } from "next/router";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -28,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%",
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -36,13 +39,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
-
-export default function PublicationForm( {pagesData} ) {
-
+export default function PublicationForm({ pagesData }) {
   var today = new Date();
   const classes = useStyles();
+  const router = useRouter();
 
   const validate = (values) => {
     const errors = {};
@@ -53,6 +53,9 @@ export default function PublicationForm( {pagesData} ) {
     if (!values.Description) {
       errors.Description = "Required";
     }
+    if (!values.PageId) {
+      errors.Description = "Required";
+    }
   };
 
   const formik = useFormik({
@@ -60,120 +63,125 @@ export default function PublicationForm( {pagesData} ) {
       Title: "",
       Description: "",
       Date: today,
-      UserId : 108,// falta por o user ID
-      PageId : 1,  // falta por o Page ID
+      UserId: "",
+      PageId: "",
     },
+
     validate,
     onSubmit: (values) => {
-      console.log(values);
+      let datas = jwt.decode(Cookies.get("token"));
+      values.UserId = datas.Id;
 
       axios
-      .post("http://localhost:3001/posts", values, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${Cookies.get('token')}`,
-      },
-    })
-      .then(function (response) {
-        console.log(response);             
-        router.push("/");
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        .post("http://localhost:3001/posts", values, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        })
+        .then(function (response) {
+          console.log(response);
+          router.push("/");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
-
   });
   return (
     <div>
-    <Navbar />
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <CreateIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-        Create a post
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={formik.handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Autocomplete
-
-                  options={pagesData}
-                  classes={{
-                    option: classes.option,
-                  }}
-                  getOptionLabel={(option) => option.Name}
-                  renderOption={(option) => (
-                    <React.Fragment>
-                      {option.Name} ({option.Id })   
-                    </React.Fragment>
-                  )
-                  }     
-                renderInput={(params) => 
-                 
-                  <TextField {...params} 
-                  margin="normal" 
-                  id="page"
-                  name="page"
-                  label="Page"                
-                  value={formik.values.PageId}
-                  onChange={formik.handleChange}
-                  />
-
-                }
-                />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="Title"
-                name="Title"
-                variant="outlined"
-                required
-                fullWidth
-                id="Title"
-                label="Title"
-                autoFocus
-                value={formik.values.Title}
-                onChange={formik.handleChange}
-              />
-              {formik.errors.Title ? <div>{formik.errors.Title}</div> : null}
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                multiline
-                rows={7}
-                id="Description"
-                label="Description"
-                name="Description"
-                autoComplete="Description"
-                value={formik.values.Description}
-                onChange={formik.handleChange}
-                fullWidth
-              />
-              {formik.errors.Description ? <div>{formik.errors.Description}</div> : null}
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
+      <Navbar />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <CreateIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Create a post
+          </Typography>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={formik.handleSubmit}
           >
-            Post
-          </Button>
-        </form>
-      </div>
-    </Container>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControl
+                  variant="outlined"
+                  className={classes.formControl}
+                  fullWidth
+                >
+                  <InputLabel id="page">Page</InputLabel>
+                  <Select
+                    labelId="page"
+                    id="page"
+                    name="PageId"
+                    label="Page"
+                    value={formik.values.PageId}
+                    onChange={formik.handleChange}
+                  >
+                    <MenuItem value={null}>none</MenuItem>
+                    {pagesData.map((e, i) => (
+                      <MenuItem key={i} value={e.Id}>
+                        {e.Name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                {formik.errors.PageId ? (
+                  <div>{formik.errors.PageId}</div>
+                ) : null}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="Title"
+                  name="Title"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="Title"
+                  label="Title"
+                  autoFocus
+                  value={formik.values.Title}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.Title ? <div>{formik.errors.Title}</div> : null}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  multiline
+                  rows={7}
+                  id="Description"
+                  label="Description"
+                  name="Description"
+                  autoComplete="Description"
+                  value={formik.values.Description}
+                  onChange={formik.handleChange}
+                  fullWidth
+                />
+                {formik.errors.Description ? (
+                  <div>{formik.errors.Description}</div>
+                ) : null}
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Post
+            </Button>
+          </form>
+        </div>
+      </Container>
     </div>
   );
-
 }
 PublicationForm.getInitialProps = async () => {
   const res = await fetch("http://localhost:3001/pages");
