59ee74df365dfd0abe1ee25b0bab508e3ab07680
BrunoFerr <58642813+BrunoFerr@users.noreply.github.com>
2021-01-24 00:57:02 +0000

    Web new features in posts

---
+++ app_Web/src/components/NoPosts.js
@@ -0,0 +1,11 @@
+import { Button, Container, CssBaseline, Typography } from "@material-ui/core";
+
+const NoPost = () => {
+  return (
+    <Container>
+      <CssBaseline />
+      <Typography>No Post Found :/</Typography>
+    </Container>
+  );
+};
+export default NoPost;
